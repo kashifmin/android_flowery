3b7d1bfc70b16550b917a5dabc9e64a9efd31136
Kashif Minhaj <dailypctips@gmail.com>
2017-11-13 19:25:52 +0530

    initial commit

---
+++ app/src/main/java/me/kashifminhaj/flowery/activities/MainActivity.java
@@ -0,0 +1,197 @@
+package me.kashifminhaj.flowery.activities;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import me.kashifminhaj.flowery.R;
+import me.kashifminhaj.flowery.utils.Classifier;
+import me.kashifminhaj.flowery.utils.Constants;
+import me.kashifminhaj.flowery.utils.TensorFlowImageClassifier;
+
+public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_TAKE_PHOTO = 2;
+    private static final int PERMISSIONS_REQUEST = 1;
+
+    private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
+    private static final String PERMISSION_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+    String mCurrentPhotoPath;
+    Bitmap selectedImage;
+
+    ImageView mImageView;
+    Button mCaptureBtn;
+    TextView resultTv;
+
+    Classifier classifier;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(!hasPermission()) {
+            requestPermission();
+        }
+
+
+
+        mImageView = findViewById(R.id.image_view);
+        mCaptureBtn = findViewById(R.id.btn_select_image);
+        resultTv = findViewById(R.id.prediction_result);
+
+        initClassifier();
+
+        mCaptureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            setPic();
+
+            makePredictions();
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        getPackageName(),
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        selectedImage = bitmap;
+        mImageView.setImageBitmap(bitmap);
+    }
+
+    private boolean hasPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkSelfPermission(PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(PERMISSION_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return true;
+        }
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (shouldShowRequestPermissionRationale(PERMISSION_CAMERA) || shouldShowRequestPermissionRationale(PERMISSION_STORAGE)) {
+                Toast.makeText(this, "Camera AND storage permission are required for this demo", Toast.LENGTH_LONG).show();
+            }
+            requestPermissions(new String[] {PERMISSION_CAMERA, PERMISSION_STORAGE}, PERMISSIONS_REQUEST);
+        }
+    }
+
+    private void initClassifier() {
+        classifier = TensorFlowImageClassifier.create(
+                getAssets(),
+                Constants.MODEL_FILENAME,
+                Constants.LABEL_FILENAME,
+                Constants.INPUT_IMAGE_SIZE,
+                Constants.IMAGE_MEAN,
+                Constants.IMAGE_STD,
+                Constants.INPUT_LAYER_NAME,
+                Constants.OUTPUT_LAYER_NAME
+        );
+    }
+
+    private void makePredictions() {
+        Bitmap resized = getResizedBitmap(selectedImage, Constants.INPUT_IMAGE_SIZE, Constants.INPUT_IMAGE_SIZE);
+        resultTv.setText(classifier.recognizeImage(resized).toString());
+    }
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(
+                bm, 0, 0, width, height, matrix, false);
+        //bm.recycle();
+        return resizedBitmap;
+    }
+}
---
+++ app/src/main/java/me/kashifminhaj/flowery/utils/Constants.java
@@ -0,0 +1,16 @@
+package me.kashifminhaj.flowery.utils;
+
+/**
+ * Created by kashif on 13/11/17.
+ */
+
+public class Constants {
+    public static final String MODEL_FILENAME = "graph.pb";
+    public static final String LABEL_FILENAME = "labels.txt";
+    public static final int INPUT_IMAGE_SIZE = 224;
+    public static final String INPUT_LAYER_NAME = "input";
+    public static final String OUTPUT_LAYER_NAME = "final_result";
+    public static final int IMAGE_MEAN = 128;
+    public static final float IMAGE_STD = 128.0f;
+
+}
